1b05a4345ba21f798ff71bdf191721208090e972
charliejuc <charliejuc@gmail.com>
2020-06-22 19:06:43 +0200

    Prepares project.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     presets: [
         ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
+        '@babel/preset-typescript'
     ],
     plugins: [
         [
@@ -9,9 +9,9 @@ module.exports = {
             {
                 // root: './src',
                 alias: {
-                    '@': './src',
-                },
-            },
-        ],
-    ],
+                    '@': './src'
+                }
+            }
+        ]
+    ]
 }
